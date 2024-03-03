a03154e38de2f9e4525f70b0494fbb02a12ea1fb
JamilServices <jamilservicos@gmail.com>
2024-03-03 16:49:26 -0300

    ✨ feat: add new type definition file for DomStore module

---
+++ dom-module/types.d.ts
@@ -0,0 +1,17 @@
+/**
+ * The DomStore module, providing an interface to manage DOM elements and integrate version-supported dependencies.
+ * It exposes methods for extending the store, adding, retrieving, and removing DOM elements.
+ *
+ * @exports DomStore
+ */
+export const DomStore: Readonly<{
+    name: string;
+    version: string;
+    domStoreExtends: (data: object) => void;
+    addElement: (element: {
+        key: string;
+        value: HTMLElement;
+    }) => void;
+    getElement: (key: string) => HTMLElement;
+    removeElement: (key: string, mode?: number) => void;
+}>;
