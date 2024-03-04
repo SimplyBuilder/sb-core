c1530114e0bf2b0b9769eeab0debf2f65a32fad9
JamilServices <jamilservicos@gmail.com>
2024-03-04 18:39:04 -0300

    ✨ feat(dom-module): add new methods 'createHTMLElement', 'createSVGElement', 'createFromStruct'
    - Added new methods to create HTML and SVG elements
    - Improved DOM module functionality for element creation and manipulation

--- dom-module/tests/index.js
+++ dom-module/tests/index.js
@@ -5,25 +5,39 @@ import {deepEqual, equal, notEqual, ok} from "node:assert";
 import {DomModule} from "#sb-core-dom-module";
 
 import { createRequire } from "node:module";
+
+import {JSDOM} from "jsdom";
+
 const require = createRequire(import.meta.url);
 const pkg = require("../package.json");
 
-const testElement = "<div></div>";
 const testKeyElement = "test.element";
+
+const dom = new JSDOM(`<!DOCTYPE html><html lang="en"><head><title>test</title></head><body></body></html>`);
+global['window'] = dom.window;
+global['document'] = dom.window.document;
+global['HTMLElement'] = dom.window.HTMLElement;
+global['SVGElement'] = dom.window.SVGElement;
+const testElement = document.createElement("div");
+
 describe("import and tests", () => {
     it("check object", () => {
         ok(typeof DomModule === "object");
         //
         deepEqual(Object.keys(DomModule), ['name', 'version',
-            'domStoreExtends', 'addElementToStore',
-            'getElementFromStore', 'removeElementFromStore']);
+            'domModuleExtends', 'createHTMLElement', 'createSVGElement',
+            'addElementToStore', 'getElementFromStore', 'removeElementFromStore',
+            'createFromStruct']);
         //
         ok(typeof DomModule.name === "string");
         ok(typeof DomModule.version === "string");
-        ok(typeof DomModule.domStoreExtends === "function");
+        ok(typeof DomModule.domModuleExtends === "function");
+        ok(typeof DomModule.createHTMLElement === "function");
+        ok(typeof DomModule.createSVGElement === "function");
         ok(typeof DomModule.addElementToStore === "function");
         ok(typeof DomModule.getElementFromStore === "function");
         ok(typeof DomModule.removeElementFromStore === "function");
+        ok(typeof DomModule.createFromStruct === "function");
     });
     it("check name and version", () => {
         equal(DomModule.name, "DomModule");
@@ -35,10 +49,36 @@ describe("import and tests", () => {
         DomModule.removeElementFromStore(testKeyElement);
         notEqual(DomModule.getElementFromStore(testKeyElement), testElement);
     });
-    it("domStoreExtends", () => {
-        notEqual(DomModule.domStoreExtends({name: "EventModule", version: "0.0.1"}), true);
-        equal(DomModule.domStoreExtends({name: "EventModule", version: "1"}), true);
-        equal(DomModule.domStoreExtends({name: "EventModule", version: "1.0.1"}), true);
-        notEqual(DomModule.domStoreExtends({name: "EventModule", version: "0.1.1"}), true);
+    it("domModuleExtends", () => {
+        notEqual(DomModule.domModuleExtends({name: "EventModule", version: "0.0.1"}), true);
+        equal(DomModule.domModuleExtends({name: "EventModule", version: "1"}), true);
+        equal(DomModule.domModuleExtends({name: "EventModule", version: "1.0.1"}), true);
+        notEqual(DomModule.domModuleExtends({name: "EventModule", version: "0.1.1"}), true);
+    });
+    it("createHTMLElement and createSVGElement", () => {
+        //DomModule.createHTMLElement({parent: testElement, element});
+        //DomModule.createSVGElement({parent: testElement, element, shadow});
+    });
+    it("createFromStruct", () => {
+        const result = DomModule.createFromStruct({
+            struct: {
+                "element": "section",
+                "attr": {
+                    "id": "test-create",
+                },
+                "dataset": {
+                    "state": "simply-builder.main"
+                },
+            }
+        });
+        ok(result);
+        const element = document.getElementById("test-create");
+        ok(typeof element === "object");
+        ok(typeof element.dataset === "object");
+        ok(element instanceof HTMLElement);
+        equal(element.dataset.state, "simply-builder.main");
+        equal(element.tagName.toLowerCase(), "section");
+        element.remove();
+        equal(document.getElementById("test-create"), null);
     });
 });
