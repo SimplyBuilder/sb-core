3b8192baa3cac5217915d53ac73da04064c6012d
JamilServices <jamilservicos@gmail.com>
2024-03-04 15:37:08 -0300

    ✨ feat(dom-module): refactor store.js
    Refactored store.js to rename functions for clarity and consistency in the DomStoreModule. Removed unnecessary comments and improved code readability.

--- dom-module/src/store.js
+++ dom-module/src/store.js
@@ -1,27 +1,19 @@
 'use strict';
 
 /**
- * Represents a simple object for storing references to DOM elements.
- * This internal storage mechanism allows for efficient tracking and management
- * of DOM elements by associating them with unique keys.
- *
  * @private
  * @typedef {Object} DomStoreElementRef
  * @memberof module:DomStoreModule
  */
 const ElementRefStore = {};
 /**
- * Adds a DOM element to the ElementRefStore with the specified key.
- * This function allows for the association of a DOM element with a unique key
- * within the store, facilitating later retrieval or manipulation of the element.
- *
- * @function addElement
+ * @function addElementToStore
  * @memberof module:DomStoreModule
  * @param {Object} data - The object containing the key and the DOM element to store.
  * @param {string} data.key - The key under which to store the element.
  * @param {HTMLElement} data.value - The DOM element to be stored.
  */
-const addElement = (data) => {
+const addElementToStore = (data) => {
    try {
       const {key, value} = data;
       if(key && value) ElementRefStore[key.toString()] = value;
@@ -31,16 +23,12 @@ const addElement = (data) => {
    }
 };
 /**
- * Retrieves a DOM element from the ElementRefStore using the specified key.
- * This function provides access to a previously stored DOM element by using its associated key,
- * enabling easy manipulation or examination of the element.
- *
- * @function getElement
+ * @function getElementFromStore
  * @memberof module:DomStoreModule
  * @param {string} key - The key of the element to retrieve.
  * @returns {HTMLElement|undefined} - The retrieved DOM element or undefined if not found or on error.
  */
-const getElement = (key) => {
+const getElementFromStore = (key) => {
    try {
       if(key) return ElementRefStore[key];
    } catch (err) {
@@ -50,18 +38,14 @@ const getElement = (key) => {
    return undefined;
 };
 /**
- * Removes a DOM element from the ElementRefStore and optionally removes associated events.
- * This function facilitates the removal of a DOM element from the store based on its key.
- * It also supports the removal of any events associated with the element if specified.
- *
- * @function removeElement
+ * @function removeElementFromStore
  * @memberof module:DomStoreModule
  * @param {Object} data - The data object containing parameters for element removal.
  * @param {string} data.key - The key of the element to be removed.
  * @param {number} [data.mode=1] - The mode of removal, determining if associated events should also be removed.
  * @param {Object} data.EventModule - The event store object for managing associated event listeners.
  */
-const removeElement = (data = {}) => {
+const removeElementFromStore = (data = {}) => {
    const{key, mode = 1, EventModule= {}} = data;
    try {
       const modeTypes = {
@@ -83,17 +67,12 @@ const removeElement = (data = {}) => {
 };
 
 /**
- * The DomStore module provides functionality for managing references to DOM elements.
- * It includes methods for adding, retrieving, and removing elements from an internal store,
- * facilitating efficient DOM manipulation and event management. This module is intended for
- * internal use within applications or packages that require direct DOM manipulation and event handling.
- *
  * @private
  * @module DomStoreModule
  */
 const DomStoreModule = Object.freeze({
-   addElement,
-   getElement,
-   removeElement
+   addElementToStore,
+   getElementFromStore,
+   removeElementFromStore
 });
 export default DomStoreModule;
