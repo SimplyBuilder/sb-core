7b5ec1d51b4e3a410dff42138aa9b14bac707d56
JamilServices <jamilservicos@gmail.com>
2024-03-04 15:37:01 -0300

    :recycle: refactor(dom-module): update variable names and function calls in tests

--- dom-module/tests/index.js
+++ dom-module/tests/index.js
@@ -2,7 +2,7 @@
 
 import {describe, it} from "node:test";
 import {deepEqual, equal, notEqual, ok} from "node:assert";
-import {DomStore} from "#sb-core-dom-module";
+import {DomModule} from "#sb-core-dom-module";
 
 import { createRequire } from "node:module";
 const require = createRequire(import.meta.url);
@@ -12,32 +12,33 @@ const testElement = "<div></div>";
 const testKeyElement = "test.element";
 describe("import and tests", () => {
     it("check object", () => {
-        ok(typeof DomStore === "object");
+        ok(typeof DomModule === "object");
         //
-        deepEqual(Object.keys(DomStore), ['name', 'version', 'domStoreExtends', 'addElement',
-            'getElement', 'removeElement']);
+        deepEqual(Object.keys(DomModule), ['name', 'version',
+            'domStoreExtends', 'addElementToStore',
+            'getElementFromStore', 'removeElementFromStore']);
         //
-        ok(typeof DomStore.name === "string");
-        ok(typeof DomStore.version === "string");
-        ok(typeof DomStore.domStoreExtends === "function");
-        ok(typeof DomStore.addElement === "function");
-        ok(typeof DomStore.getElement === "function");
-        ok(typeof DomStore.removeElement === "function");
+        ok(typeof DomModule.name === "string");
+        ok(typeof DomModule.version === "string");
+        ok(typeof DomModule.domStoreExtends === "function");
+        ok(typeof DomModule.addElementToStore === "function");
+        ok(typeof DomModule.getElementFromStore === "function");
+        ok(typeof DomModule.removeElementFromStore === "function");
     });
     it("check name and version", () => {
-        equal(DomStore.name, "DomStore");
-        equal(DomStore.version, pkg.version);
+        equal(DomModule.name, "DomModule");
+        equal(DomModule.version, pkg.version);
     });
-    it("addElement, getElement and removeElement", () => {
-        DomStore.addElement({key: testKeyElement, value: testElement});
-        equal(DomStore.getElement(testKeyElement), testElement);
-        DomStore.removeElement(testKeyElement);
-        notEqual(DomStore.getElement(testKeyElement), testElement);
+    it("addElementToStore, getElementFromStore and removeElementFromStore", () => {
+        DomModule.addElementToStore({key: testKeyElement, value: testElement});
+        equal(DomModule.getElementFromStore(testKeyElement), testElement);
+        DomModule.removeElementFromStore(testKeyElement);
+        notEqual(DomModule.getElementFromStore(testKeyElement), testElement);
     });
     it("domStoreExtends", () => {
-        notEqual(DomStore.domStoreExtends({name: "EventModule", version: "0.0.1"}), true);
-        equal(DomStore.domStoreExtends({name: "EventModule", version: "1"}), true);
-        equal(DomStore.domStoreExtends({name: "EventModule", version: "1.0.1"}), true);
-        notEqual(DomStore.domStoreExtends({name: "EventModule", version: "0.1.1"}), true);
+        notEqual(DomModule.domStoreExtends({name: "EventModule", version: "0.0.1"}), true);
+        equal(DomModule.domStoreExtends({name: "EventModule", version: "1"}), true);
+        equal(DomModule.domStoreExtends({name: "EventModule", version: "1.0.1"}), true);
+        notEqual(DomModule.domStoreExtends({name: "EventModule", version: "0.1.1"}), true);
     });
 });
