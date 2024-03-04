a1714961d0e250d5d132558a63a78d9ff3172bd7
JamilServices <jamilservicos@gmail.com>
2024-03-04 08:55:02 -0300

    ✨ refactor(event): remove unnecessary comments and descriptions in component.js

--- event-module/src/component.js
+++ event-module/src/component.js
@@ -1,31 +1,18 @@
 'use strict';
 
 /**
- * Stores the custom event types.
- *
- * This object is used to keep track of all registered custom event types within the application.
- *
  * @private
  * @module EventTypes
  * @memberof module:EventComponentModule
  */
 const EventTypes = {};
 /**
- * Stores the actions associated with custom event types.
- *
- * This object is used to store functions or actions that are triggered when custom events occur.
- *
  * @private
  * @module EventActions
  * @memberof module:EventComponentModule
  */
 const EventActions = {};
 /**
- * Registers a custom event type and its associated action.
- *
- * This function allows for the dynamic registration of custom event types and their corresponding actions.
- * If the event type does not already exist, it's added to EventTypes, and the action is stored in EventActions.
- *
  * @function eventRegister
  * @memberof module:EventComponentModule
  * @param {string} name - The name of the custom event type to register.
@@ -44,10 +31,6 @@ const eventRegister = (name, fn = undefined) => {
     }
 };
 /**
- * Unregisters a custom event type and its associated action.
- *
- * This function removes a previously registered custom event type and its action from EventTypes and EventActions.
- *
  * @function eventUnregister
  * @memberof module:EventComponentModule
  * @param {string} name - The name of the custom event type to unregister.
@@ -66,11 +49,6 @@ const eventUnregister = (name) => {
     }
 };
 /**
- * The EventComponent module provides functionality for managing custom events and actions.
- *
- * It includes methods for registering and unregistering custom event types and their associated actions.
- * This module is intended for internal use within the application or package.
- *
  * @private
  * @module EventComponentModule
  */
