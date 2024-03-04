58af781bf2c72552e8f3d6b5b36949b1cf3ee2f9
JamilServices <jamilservicos@gmail.com>
2024-03-04 09:05:07 -0300

    🔨 feat(event-module): add return statements to eventRegister and eventUnregister functions

--- event-module/src/component.js
+++ event-module/src/component.js
@@ -17,6 +17,7 @@ const EventActions = {};
  * @memberof module:EventComponentModule
  * @param {string} name - The name of the custom event type to register.
  * @param {function} [fn=undefined] - The function or action to associate with the event type.
+ * @returns {boolean}
  */
 const eventRegister = (name, fn = undefined) => {
     try {
@@ -24,11 +25,13 @@ const eventRegister = (name, fn = undefined) => {
             if(typeof EventTypes[name.toString().toUpperCase()] === "undefined") {
                 EventTypes[name.toString().toUpperCase()] = name.toString();
                 EventActions[EventTypes[name.toString().toUpperCase()]] = fn;
-            }
+                return true;
+            } else console.error("Event: "+ name +" already exists registered!");
         }
     } catch (err) {
         console.error(err);
     }
+    return false;
 };
 /**
  * @function eventUnregister
@@ -42,11 +45,13 @@ const eventUnregister = (name) => {
             if (eventType) {
                 delete EventTypes[name.toString().toUpperCase()];
                 if (EventActions[eventType]) delete EventActions[eventType];
+                return true;
             }
         }
     } catch (err) {
         console.error(err);
     }
+    return false;
 };
 /**
  * @private
