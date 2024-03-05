07f6a3e0a9c92dd1957451cea63bbb2702e6f4a4
JamilServices <jamilservicos@gmail.com>
2024-03-05 08:17:09 -0300

    ✨ feat(event-module): add rollup config for EventModule
    Created rollup-core.config.js to configure the build process for the EventModule library. Included input/output settings, plugins for replacement and terser, and default output properties.

---
+++ event-module/rollup-core.config.js
@@ -0,0 +1,55 @@
+'use strict';
+
+import replace from '@rollup/plugin-replace';
+import terser from '@rollup/plugin-terser';
+
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const pkg = require("./package.json");
+
+const libName = 'EventModule';
+const {version} = pkg;
+
+const outputDefault = {
+    name: libName,
+    format: 'module',
+    strict: true,
+    generatedCode: {
+        preset: 'es5',
+        arrowFunctions: true,
+        constBindings: true,
+        objectShorthand: true,
+        symbols: true,
+    },
+    extend: true,
+    validate: true,
+    compact: true
+};
+export default {
+    input: 'src/main.js',
+    output: [
+        {
+            file: 'lib/main.js',
+            ...outputDefault
+        },
+        {
+            file: 'lib/main.min.js',
+            plugins: [
+                terser({
+                    module: true,
+                    ecma: 5,
+                    keep_classnames: true,
+                    keep_fnames: true,
+                })
+            ],
+            ...outputDefault
+        }
+    ],
+    plugins: [
+        replace({
+            preventAssignment:true,
+            'EventModuleLibName': libName,
+            'EventModuleLibVersion': version
+        })
+    ]
+};
